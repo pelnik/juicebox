d080d7c0c30fadfd2755b06cd32b86c9776c430b
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-03-01 11:29:20 -0600

    database set

---
+++ db/seed.js
@@ -0,0 +1,17 @@
+const { client } = require("./index");
+
+async function testDB() {
+  try {
+    client.connect();
+
+    const { rows } = await client.query(`SELECT * FROM users;`);
+
+    console.log(rows);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+testDB();
