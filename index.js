b7affb3804daf66ae95e70dd134e66f8024c66f1
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-03 09:29:27 -0600

    connecting to database

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const PORT = 3000;
 const express = require("express");
 const apiRouter = require("./api");
 const app = express();
+const morgan = require('morgan');
+const { client } = require('./db');
 
+app.use(morgan('dev'));
+app.use(express.json());
 app.use("/api", apiRouter);
+
+client.connect();
 
 app.listen(PORT, () => {
   console.log("the server is up on port", PORT);
