9ec99646d9d57c9b62fe741e992fce3176a0ed85
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-03 09:42:40 -0600

    added post api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const apiRouter = express.Router();
 const usersRouter = require("./users");
+const postRouter = require('./posts');
 
 apiRouter.use("/users", usersRouter);
+apiRouter.use('/posts', postRouter);
+
 
 module.exports = apiRouter;
---
+++ api/posts.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const postRouter = express.Router();
+const { getAllPosts } = require('../db');
+
+
+postRouter.use((req, res, next) => {
+  console.log("A request is being made to /posts");
+
+  next();
+});
+
+postRouter.get('/', async (req, res, next) => {
+  try {
+    const posts = await getAllPosts();
+  } catch (error) {
+    console.error('Error on get all posts', error)
+  }
+
+
+  res.send({
+    posts,
+  });
+});
+
+module.exports = postRouter;
