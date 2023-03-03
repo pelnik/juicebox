31dbfa258d1b8bd9f2301e379dba2f41b8ed7a51
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-03 10:45:57 -0600

    add JWT and authorization logic

--- api/index.js
+++ api/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
 const apiRouter = express.Router();
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
+apiRouter.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({name, message}) {
+      next({ name, message })
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    })
+  }
+})
+
 const usersRouter = require("./users");
 const postRouter = require("./posts");
 const tagsRouter = require("./tags");
@@ -7,5 +38,12 @@ const tagsRouter = require("./tags");
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/posts", postRouter);
 apiRouter.use("/tags", tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+    message: error.message,
+  })
+})
 
 module.exports = apiRouter;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 const PORT = 3000;
 const express = require("express");
 const apiRouter = require("./api");
 const app = express();
 const morgan = require('morgan');
 const { client } = require('./db');
+
 
 app.use(morgan('dev'));
 app.use(express.json());
