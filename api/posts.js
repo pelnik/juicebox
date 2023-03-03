98abb4a283ac4642693db84d10e768ec2ad04df8
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-03-03 14:23:23 -0600

    tags

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const postRouter = express.Router();
-const { getAllPosts } = require("../db");
+const { getAllPosts, createPost } = require("../db");
+const { requireUser } = require("./utils");
+
+postRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+  try {
+    postData.title = title;
+    postData.content = content;
+    postData.authorId = req.user.id;
+
+    const post = await createPost(postData);
+    if (post) {
+      restart.send({ post });
+    } else {
+      next(error);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+
+  res.send({ message: "under construction" });
+});
 
 postRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
