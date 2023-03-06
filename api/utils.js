73bb58b8040886e30f7e6fa736af3c7f6a6e903c
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-06 14:11:31 -0600

    update requireActiveUser

--- api/utils.js
+++ api/utils.js
@@ -11,29 +11,11 @@ function requireUser(req, res, next) {
   next();
 }
 
-async function requireActiveUser(req, res, next) {
-  try {
-    const user = await getUserById(req.user.id);
-
-    if (!user.active) {
-      next({
-        name: "NonactiveUserError",
-        message: "User must be active to perform this action",
-      });
-    }
-
-    next();
-  } catch ({message, name}) {
+function requireActiveUser(req, res, next) {
+  if (!req.user.active) {
     next({
-      message,
-      name
-    });
-  }
-
-  if (!req.user) {
-    next({
-      name: "MissingUserError",
-      message: "You must be logged in to perform this action",
+      name: "NonactiveUserError",
+      message: "User must be active to perform this action",
     });
   }
 
